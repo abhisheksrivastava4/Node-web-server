e9ccedd64d4aa74db177eb6283f26c0df2126129
Abhishek Srivastava <abhishek.srivastava4@gmail.com>
2018-06-25 15:07:41 +0530

    abhi

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var hbs = require('hbs');
+fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+console.log(__dirname+'views/partials');
+app.set('view engine','hbs');
+
+hbs.registerHelper('screamIt',(text) => {
+ return text;
+});
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+})
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req, res, next)=>{
+var now = new Date().toString();
+var log = `${now}: ${req.method} ${req.url}`;    
+fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err)
+    console.log('unable to open file');
+});    
+next();
+});
+
+app.get('/',(req, res)=>{
+    res.render('Home.hbs',{
+    PageTitle:'Home Page',   
+    WelcomeMessage: 'My Web Site'
+});
+});
+
+app.get('/about',(req, res) => {
+    res.render('about.hbs',{
+        PageTitle: 'About Title'
+    });
+});
+
+app.get('/bad',(req, res) => {
+    res.send({
+        error: 'Unable to handle the request'
+    });
+});
+
+app.listen('5000',()=>{
+    console.log('server up on port 5000');
+});
